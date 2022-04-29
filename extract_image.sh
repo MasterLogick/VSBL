11fdc093249cb875ff562bd27fb49eff3a66c38a
MasterLogick <31408232+masterlogick@users.noreply.github.com>
2022-04-29 23:38:55 +0300

    migrate building system to cmake

--- extract_image.sh
+++ extract_image.sh
@@ -1,6 +1,7 @@
-objcopy -O binary "$1"/"$2" "$1"/"$3".old
-REAL_SIZE=$(stat -c%s "$1"/"$3".old)
-BLOCK_COUNT=$(((REAL_SIZE + $4 - 1) / $4))
-BLOCK_COUNT=$((BLOCK_COUNT > $(($5)) ? BLOCK_COUNT : $(($5)) / $4))
-dd if=/dev/zero of="$1"/"$3" bs="$4" count=$BLOCK_COUNT
-dd if="$1"/"$3".old of="$1"/"$3" bs=1 count="$REAL_SIZE" seek=0 conv=notrunc
+objcopy -O binary "$1" "$2".old
+REAL_SIZE=$(stat -c%s "$2".old)
+BLOCK_COUNT=$(((REAL_SIZE + $3 - 1) / $3))
+BLOCK_COUNT=$((BLOCK_COUNT > $((1024*1024)) ? BLOCK_COUNT : $((1024*1024)) / $3))
+dd if=/dev/zero of="$2" bs="$3" count=$BLOCK_COUNT
+dd if="$2".old of="$2" bs=1 count="$REAL_SIZE" seek=0 conv=notrunc
+rm "$2".old
